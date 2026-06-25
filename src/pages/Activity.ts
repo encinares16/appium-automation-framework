4f8bd889fd91850ff98c8792ba735f838cbe46b8
Rome <jerome.encinares@apolloglobal.net>
2026-06-25 15:07:02 +0800

    chore: add new page object model activity

---
+++ src/pages/Activity.ts
@@ -0,0 +1,25 @@
+import { driver } from '@wdio/globals'
+import { numberedSteps } from '@utils/customSteps.ts'
+import { APP_PACKAGE } from '@constants/index.contants.ts'
+import { ScreenshotParams, takeScreenshot } from '@utils/takeScreenshot.ts'
+
+class Activity {
+
+    async launchApp() {
+      await numberedSteps.start('Launch the application.', async () => {
+        await driver.activateApp(APP_PACKAGE)
+      })
+    }
+
+    async closeApp() {
+      await numberedSteps.start('Exit the application.', async () => {
+        await driver.terminateApp(APP_PACKAGE)
+      })
+    }
+
+    async screenshot(details: ScreenshotParams){
+      await takeScreenshot(details)
+    }
+}
+
+export default new Activity()
